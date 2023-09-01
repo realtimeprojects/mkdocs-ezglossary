0789fcebbbfab80a00503a45784685dbc9a788f3
Claudio Klingler <ck@realtime-projects.com>
2023-09-01 15:01:48 +0200

    add summary tests.

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -15,5 +15,86 @@ def test_summary(simple, summary, config):
     log.debug(summary)
     tree = etree.fromstring(summary)
 
+    dd = xp.dd().ul().li().a(href="../simple.md#test_first_defs_0",
+                             text="Hello")
     dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="first"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 1
+
+    dd = xp.dd().ul().li().a(href="../simple.md#test_first_refs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="first"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 0
+
+    dd = xp.dd().ul().li().a(href="../simple.md#test_third_defs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="third"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 1
+
+    dd = xp.dd().ul().li().a(href="../simple.md#demo_first_defs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="demo")
+    dl = dl.has(xp.dt(text="first"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 1
+
+    dd = xp.dd().ul().li().a(href="../simple.md#test_third_refs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="third"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 1
+
+    dd = xp.dd().ul().li().a(href="../simple.md#demo_third_refs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="demo")
+    dl = dl.has(xp.dt(text="third"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 0
+
+
+def test_summary_noref(simple, summary, config):
+    config['list_references'] = False
+    pages = mock.render([simple, summary], config)
+    summary = pages["summary.md"]
+    tree = etree.fromstring(summary)
+
+    dd = xp.dd().ul().li().a(href="../simple.md#test_first_defs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="first"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 1
+
+    dd = xp.dd().ul().li().a(href="../simple.md#test_third_refs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="third"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 0
+
+
+def test_summary_nodef(simple, summary, config):
+    config['list_definitions'] = False
+    pages = mock.render([simple, summary], config)
+    summary = pages["summary.md"]
+    tree = etree.fromstring(summary)
+
+    dd = xp.dd().ul().li().a(href="../simple.md#test_first_defs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="first"))
+    dl = dl.has(dd)
+    assert len(tree.xpath(str(dl))) == 0
+
+    dd = xp.dd().ul().li().a(href="../simple.md#test_third_refs_0",
+                             text="Hello")
+    dl = xp.dl(_class="mkdocs-glossary", _id="test")
+    dl = dl.has(xp.dt(text="third"))
+    dl = dl.has(dd)
     assert len(tree.xpath(str(dl))) == 1
