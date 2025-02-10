285ad3dbbc3be715273ca913970dc2745c837bab
Claudio Klingler <ck@realtime-projects.com>
2025-02-10 22:08:49 +0100

    Improve "en" translation rules.

--- src/mkdocs_ezglossary_plugin/plurals.py
+++ src/mkdocs_ezglossary_plugin/plurals.py
@@ -1,19 +1,30 @@
 plurals = {
     'en': {
-        's$': [''],
-        'es$': ['', 'is'],
-        'ies$': ['y'],
-        'ves$': ['f', 'fe'],
-        'ses$': [''],
-        'zes$': [''],
-        'i$': ['us'],
-        'a$': ['on'],
+        # Regular plurals
+        's$': [''],           # cats -> cat
+        'es$': ['', 'is'],    # boxes -> box, analysis -> analyses
+        'ies$': ['y'],        # cities -> city
+        'ves$': ['f', 'fe'],  # wolves -> wolf, knives -> knife
+        'ses$': ['s'],        # buses -> bus
+        'zes$': ['z'],        # quizzes -> quiz
+        
+        # Latin/Greek plurals
+        'i$': ['us'],         # fungi -> fungus
+        'ae$': ['a'],         # larvae -> larva
+        'a$': ['on', 'um'],   # phenomena -> phenomenon, data -> datum
+        
+        # Irregular plurals
         'children$': ['child'],
         'geese$': ['goose'],
-        'men': ['man'],
+        'men$': ['man'],      # also handles: women -> woman
         'teeth$': ['tooth'],
         'feet$': ['foot'],
-        'mice': ['mouse'],
-        'people': ['person'],
+        'mice$': ['mouse'],
+        'people$': ['person'],
+        'oxen$': ['ox'],
+        'indices$': ['index'],
+        'matrices$': ['matrix'],
+        'vertices$': ['vertex'],
+        'appendices$': ['appendix'],
     }
 }
